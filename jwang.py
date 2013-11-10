0198f1f9ea709b643e0c1a57ad4420b3b3d4dde2
Joyce Wang <joycewang2002@yahoo.com>
2013-11-10 11:28:03 -0500

    still testing

--- jwang.py
+++ jwang.py
@@ -1,12 +1,11 @@
 import os
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def jwang():
-    return 'Hello World'
+def home():
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run()
