386ad15279d51c4ced8e85561d200822121c43fd
Joyce Wang <joycewang2002@yahoo.com>
2013-11-10 12:00:01 -0500

    adding

--- jwang.py
+++ jwang.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,4 +8,5 @@ def home():
     return render_template('index.html')
 
 if __name__=='__main__':
-    app.run()
+    port = int(os.environ.get("PORT",2121))
+    app.run(host='0.0.0.0',port=port)
