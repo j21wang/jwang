3e64d42da404b73f13785d5c73253df3ea68ccb4
Joyce Wang <joycewang2002@yahoo.com>
2013-11-13 17:54:57 -0500

    website working

--- jwang.py
+++ jwang.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/portfolio', methods=['GET'])
+def portfolio():
+    return render_template('portfolio.html')
+
 if __name__=='__main__':
     port = int(os.environ.get("PORT",2121))
     app.run(host='0.0.0.0',port=port)
